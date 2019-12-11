a57c63075454944821c0231d7b9ea41e23bc9086
arturt10ten <arturt10ten@gmail.com>
2019-12-12 01:27:39 +0300

    about us redirect added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let start = document.getElementById('start');
 let load = document.getElementById('load');
 let options = document.getElementById('opt');
 let call_us = document.getElementById('we');
+call_us.addEventListener('click', () => {
+    location = './we.html';
+})
 let exit = document.getElementById('exit');
 exit.addEventListener('click', () => {
     close();
