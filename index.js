11c4af5c26c6ecba9ff143d4746a69e414582b37
arturt10ten <arturt10ten@gmail.com>
2019-12-12 01:25:18 +0300

    added close action

---
+++ index.js
@@ -0,0 +1,8 @@
+let start = document.getElementById('start');
+let load = document.getElementById('load');
+let options = document.getElementById('opt');
+let call_us = document.getElementById('we');
+let exit = document.getElementById('exit');
+exit.addEventListener('click', () => {
+    close();
+})
