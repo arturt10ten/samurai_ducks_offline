3b0ee585cbfb7033aca94b3c5b2cf8b3ce35787e
arturt10ten <arturt10ten@gmail.com>
2019-12-12 02:13:52 +0300

    back button added

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+let con = document.querySelector('iframe');
+let btn = document.querySelector('button');
 window.addEventListener('message', (e) => {
     console.log(e);
     if (e && e.data && e.data.event == 'close') window.close();
-})
+    if (e && e.data && e.data.event == 'away') btn.classList.remove('hidden')
+});
+btn.addEventListener('click', () => {
+    con.src = './menu.html';
+    btn.classList.add('hidden')
+});
+btn.classList.add('hidden')
